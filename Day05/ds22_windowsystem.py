9e3381c4838060f13d0966bc31a7ac2510b678a4
donghyunHong <tlxmscksals@naver.com>
2023-02-17 17:24:55 +0900

    02.17

---
+++ Day05/ds22_windowsystem.py
@@ -0,0 +1,23 @@
+# C:/windows/system32 파일 출력
+import os
+# print(dir(os))
+
+def makeFileList(folder):
+    fnameAry = []
+    for _, _, fnames in os.walk(folder):
+        for fname in fnames:
+            fnameAry.append(fname)
+
+    return fnameAry
+
+def insertionSort(ary):
+    n = len(ary)
+    for end in range(1, n):
+        for cur in range(end, 0, -1):
+            if ary[cur-1] < ary[cur]: # 내림차순으로 변경
+                ary[cur-1], ary[cur] = ary[cur], ary[cur-1]
+    return ary
+
+fileAry = makeFileList('C:/Program Files/Common Files')
+fileAry = insertionSort(fileAry)
+print('파일명 역순-->', file)
